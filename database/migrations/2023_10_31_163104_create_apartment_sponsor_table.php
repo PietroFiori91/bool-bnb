0217de7e250d097c422a19b61eff26ba8ea8b900
PietroFiori91 <pederflowers@hotmail.it>
2023-10-31 17:39:29 +0100

    make migration apartment_sponsor

---
+++ database/migrations/2023_10_31_163104_create_apartment_sponsor_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->id(); //PK ID
+
+            $table->unsignedBigInteger('apartment_id');
+            $table->unsignedBigInteger('sponsor_id');
+            $table->date('start_date');
+            $table->date('end_date');
+
+            //FK
+            $table->foreign('apartment_id')
+                ->references('id')
+                ->on('apartments')
+                ->onDelete('cascade');
+
+            $table->foreign('sponsor_id')
+                ->references('id')
+                ->on('sponsors')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_sponsor');
+    }
+};
