a44534e26e360783b9da3e3b4fd5c18fdd6eb1ee
PietroFiori91 <pederflowers@hotmail.it>
2023-11-01 20:26:54 +0100

    make image model

---
+++ app/Models/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use app\Models\Apartment;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'apartment_id', 'description', 'url', 'front_img',
+    ];
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+}
