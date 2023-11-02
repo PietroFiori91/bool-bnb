06ff6d757d0796294b37a02a85c06fd95e941839
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-11-02 17:03:07 +0100

    add "home" & "apartments" paths buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,10 @@
         <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
         <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
     </div>
+
+    <div class="container py-5">
+        <a href="/admin/apartments" class="btn btn-primary btn-lg" type="button">admin/apartments</a>
+    </div>
 </div>
 
 <div class="content">
