9e6909a936f8539fbbbbb3cc723b52e84dc2472c
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-11-02 10:14:38 +0100

    asas

--- resources/views/apartment/index.blade.php
+++ resources/views/apartment/index.blade.php
@@ -1,31 +1,32 @@
 @extends('layouts.public')
 
 @section('content')
-    <div class="container">
+    <div class="container ">
         <div class="row">
-        @foreach ($apartments as $apartment)
-            <div class="card up rounded-0">
-                <div class="card sub border-0 rounded-0">
-                    <div class="card-image">
-                        <img src="" class="card-img-top rounded-0" alt="">
-                    </div>
-                    <div class="card-body w-100">
-                        <div class="row">
-                            <div class="col-9 d-flex align-items-center">
-                                <h5 class="card-title p-0"><strong>{{ $apartment->name }}</strong></h5>
+            <div class="col-12 d-flex justify-content-center">
+                @foreach ($apartments as $apartment)
+                    <div class="card up rounded-0">
+
+                        <div class="card-image">
+                            <img src="" class="card-img-top rounded-0" alt="">
+                        </div>
+                        <div class="card-body w-100">
+                            <div class="row">
+                                <div class="col-9 d-flex align-items-center">
+                                    <h5 class="card-title p-0"><strong>{{ $apartment->name }}</strong></h5>
+                                </div>
+                                <div class="col-3 d-flex align-items-start">
+                                    <span class="text-decoration-underline">{{ $apartment->address }}</span>
+                                </div>
                             </div>
-                            <div class="col-3 d-flex align-items-start">
-                                <span class="text-decoration-underline">{{ $apartment->address }}</span>
-                            </div>
+
+                            <p class="card-text hidden-on-hover"></p>
+                            <p class="card-text instructions"></p>
                         </div>
 
-                        <p class="card-text hidden-on-hover"></p>
-                        <p class="card-text instructions"></p>
-                    </div>
 
-                </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-        
-    </div>
-@endsection
+        </div>
+    @endsection
