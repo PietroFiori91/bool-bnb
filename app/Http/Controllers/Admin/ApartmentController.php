b21c9ace16aff6215feac138276fa0c004a93c1c
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-11-02 15:04:04 +0100

    move directory "apartments" in "admin" + fix route

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -15,7 +15,7 @@ class ApartmentController extends Controller
         $apartments = Apartment::all();
       
         // indirizza i nostri dati alla view index 
-        return view("apartment.index", ["apartments" => $apartments]);
+        return view("admin.apartments.index", ["apartments" => $apartments]);
     }
 
     /**
@@ -23,7 +23,7 @@ class ApartmentController extends Controller
      */
     public function create()
     {
-        return view('apartment.create');
+        return view('admin.apartments.create');
     }
 
     /**
