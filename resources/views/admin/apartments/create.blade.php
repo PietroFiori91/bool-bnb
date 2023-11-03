b8f27126c4052565817722938faa3c3829df9750
PietroFiori91 <pederflowers@hotmail.it>
2023-11-03 14:58:33 +0100

    add error crud

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -5,50 +5,83 @@
         <div class="row">
             <div class="col">
                 <form action="{{ route('admin.apartments.store') }}" method="POST">
-                    @csrf()
+                    @csrf
                     <div class="mb-3">
                         <label class="form-label">Name: </label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="name">
+                        @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Descrizione:</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" name="description" rows="3"></textarea>
+                        @error('description')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Indirizzo:</label>
                         <input class="form-control" id="exampleFormControlTextarea1" name="address">
+                        @error('address')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Numero camere:</label>
                         <input class="form-control" type="number" id="exampleFormControlTextarea1" name="room">
+                        @error('room')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Numero letti:</label>
-                        <input class="form-control" type="number" id="exampleFormControlTextarea1" name="bed">
+                        <input class= "form-control" type="number" id="exampleFormControlTextarea1" name="bed">
+                        @error('bed')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
-                        <label class="form-label">Numero bagni</label>
+                        <label class="form-label">Numero bagni:</label>
                         <input class="form-control" type="number" id="exampleFormControlTextarea1" name="bathroom">
+                        @error('bathroom')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Metratura: </label>
                         <input type="number" class="form-control" id="exampleFormControlInput1" name="mq">
+                        @error('mq')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Latitudine:</label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="latitude">
+                        @error('latitude')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Longitudine:</label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="longitude">
+                        @error('longitude')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Visibile: </label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="visibility">
+                        @error('visibility')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Disponibile: </label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="availability">
+                        @error('availability')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
 
                     <button class="btn btn-primary">Salva</button>
--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -6,50 +6,84 @@
             <div class="col">
                 <form action="{{ route('admin.apartments.update', $apartments->id) }}" method="POST">
                     @csrf()
-                    @method("put")
+                    @method('put')
                     <div class="mb-3">
                         <label class="form-label">Name: </label>
-                        <input type="text" class="form-control" value="{{$apartments->name}}" name="name">
+                        <input type="text" class="form-control" value="{{ $apartments->name }}" name="name">
+                        @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Descrizione:</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" name="description" rows="3">{{$apartments->description}}</textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="description" rows="3">{{ $apartments->description }}</textarea>
+                        @error('description')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Indirizzo:</label>
-                        <input class="form-control" value="{{$apartments->address}}" name="address">
+                        <input class="form-control" value="{{ $apartments->address }}" name="address">
+                        @error('address')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Numero camere:</label>
-                        <input class="form-control" type="number" value="{{$apartments->room}}" name="room">
+                        <input class="form-control" type="number" value="{{ $apartments->room }}" name="room">
+                        @error('room')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Numero letti:</label>
-                        <input class="form-control" type="number" value="{{$apartments->bed}}" name="bed">
+                        <input class="form-control" type="number" value="{{ $apartments->bed }}" name="bed">
+                        @error('bed')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Numero bagni</label>
-                        <input class="form-control" type="number" value="{{$apartments->bathroom}}" name="bathroom">
+                        <input class="form-control" type="number" value="{{ $apartments->bathroom }}" name="bathroom">
+                        @error('bathroom')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Metratura: </label>
-                        <input type="number" class="form-control" value="{{$apartments->mq}}" name="mq">
+                        <input type="number" class="form-control" value="{{ $apartments->mq }}" name="mq">
+                        @error('mq')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Latitudine:</label>
-                        <input type="text" class="form-control" value="{{$apartments->latitude}}" name="latitude">
+                        <input type="text" class="form-control" value="{{ $apartments->latitude }}" name="latitude">
+                        @error('latitude')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Longitudine:</label>
-                        <input type="text" class="form-control" value="{{$apartments->longitude}}" name="longitude">
+                        <input type="text" class="form-control" value="{{ $apartments->longitude }}" name="longitude">
+                        @error('longitude')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Visibile: </label>
-                        <input type="text" class="form-control" value="{{$apartments->visibility}}" name="visibility">
+                        <input type="text" class="form-control" value="{{ $apartments->visibility }}" name="visibility">
+                        @error('visibility')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Disponibile: </label>
-                        <input type="text" class="form-control" value="{{$apartments->availability}}" name="availability">
+                        <input type="text" class="form-control" value="{{ $apartments->availability }}"
+                            name="availability">
+                        @error('availability')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
 
                     <button class="btn btn-primary">Salva</button>
