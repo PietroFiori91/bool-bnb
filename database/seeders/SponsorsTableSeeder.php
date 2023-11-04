9bcf5938e70c7b607851c530f3fd67c53ef788a9
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-11-04 11:44:34 +0100

    fix sponsor table seeder

--- database/seeders/SponsorsTableSeeder.php
+++ database/seeders/SponsorsTableSeeder.php
@@ -6,30 +6,45 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Sponsor;
 
+
+
 class SponsorsTableSeeder extends Seeder
 {
+    private $sponsors =  [
+        [
+            "name" => "Economy",
+            "price" => "2.99",
+            "duration" => "24",
+            "payment_report" => ""
+        ],
+        [
+            "name" => "Standard",
+            "price" => "4.99",
+            "duration" => "72",
+            "payment_report" => ""
+        ],
+        [
+            "name" => "Premium",
+            "price" => "8.99",
+            "duration" => "144",
+            "payment_report" => ""
+        ]
+
+
+
+    ];
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Sponsor::create([
-            "name"=>"Economy",
-            "price"=>"2.99",
-            "duration"=> 1,
-            "payment_report"=>"dato a caso"
-        ]);
-        Sponsor::create([
-            "name"=>"Standard",
-            "price"=>"4.99",
-            "duration"=> 3,
-            "payment_report"=>"dato a caso"
-        ]);
-        Sponsor::create([
-            "name"=>"Premium",
-            "price"=>"8.99",
-            "duration"=> 6,
-            "payment_report"=>"dato a caso"
-        ]);
+        foreach ($this->sponsors as  $sponsor) {
+            $newSponsor = new Sponsor();
+            $newSponsor->name = $sponsor["name"];
+            $newSponsor->price = $sponsor["price"];
+            $newSponsor->duration = $sponsor["duration"];
+            $newSponsor->payment_report = $sponsor["payment_report"];
+            $newSponsor->save();
+        }
     }
 }
