2705ce8e41b4507438ed24604440ccb7085b2a7c
masterkudi <cudini.andrea@gmail.com>
2023-11-06 15:26:36 +0100

    aggiunta config braintree

---
+++ config/boolbnb/braintree.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'env' => env('BRAINTREE_ENV', 'sandbox'),
+    'merchant_id' => env('BRAINTREE_MERCHANT_ID', 'w5n35p6kks26jwmv'),
+    'public_key' => env('BRAINTREE_PUBLIC_KEY', '2x656nwvmyxgxcwc'),
+    'private_key' => env('BRAINTREE_PRIVATE_KEY', 'bb8bf069030c57fb46ec5d7abc56e80d'),
+];
